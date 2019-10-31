bf29a7f259203ad19b302302fa0f9e98913adc23
Richard Jean-Baptiste <Richinbk1@gmail.com>
2019-10-30 22:09:55 -0400

    intial commit

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 module.exports={
     
     
-consumer_key:         'XjLaARltB5c9QMNQSRWI8pVRL',
-  consumer_secret:      'jch6jYJC2MUUqtfQ7abFgSJkAv5oQooAzBXTRiyy0JUnfS8MNJ',
-  access_token:         '513608117-GmDIxjRqD31q6JblnMIwXCcuMJ1W48Q5kwmdcznK',
-  access_token_secret:  'MwxuWjbaZDf981SUB4SSSCRUNzmYoifyNAXVLYsWdjs4Q',
+  consumer_key:         'nFPFl3fBLELG4UBj57KyKLCLE ',
+  consumer_secret:      '6NhXvmlJugPx2opM894XjpEiyXScBrQs40D8ZCWpkexgX61tPg',
+  access_token:         '3983518996-2Csu1UihZfXtKEBn7PoeiMPa636xrKoeMaDNefj',
+  access_token_secret:  'MaVG07GRxc4rHJOZ4rx41Tp0cf50w7P7cGBAocldlcHJv',
   //timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 
 }

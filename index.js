e02c3f430a64da6824447920eb1d1b3a1131276c
Richard Jean-Baptiste <Richard@Richards-MacBook-Air.local>
2019-12-03 23:18:42 -0500

    added more quotes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const keys = require('./config');
 
 console.log('Replier bot is starting');
 
+
 const T = new Twitter(
    keys
 );
@@ -16,7 +17,7 @@ function TweetOut(){
 
    quoteNum = Math.floor(Math.random() * 38);
 
-   let tweetString = Quotes[quoteNum]['Quote'] + "\n" + Quotes[quoteNum]['Author'];
+   let tweetString = Quotes[quoteNum]['Quote'] + "\n\n - " + Quotes[quoteNum]['Author'];
 
    T.post('statuses/update', { status: tweetString }, function(err, data, response) {
       console.log(data);
@@ -30,4 +31,5 @@ setInterval(TweetOut, 1000 * 60 * 60);
 app.listen(3000);
 
 
-//console.log(Quotes[10]['Quote'] + "\n" + Quotes[10]['Author']);
+//console.log(Quotes[10]['Quote'] + "\n\n - " + Quotes[10]['Author']);
+
